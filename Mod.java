24f224e57e31708c1d5e8d574e695c57b2aba903
Jaylon <jonesjaylon725@gmail.com>
2022-04-20 12:54:03 -0500

    first commit

---
+++ Mod.java
@@ -0,0 +1,12 @@
+/**
+ * Mod
+ */
+public class Mod {
+
+    public static void main(String[] args) {
+
+        People man = new People(20, "Jaylon");
+
+        System.out.print(man.name);
+    }
+}
---
+++ People.java
@@ -0,0 +1,23 @@
+public class People {
+    int age;
+    String name;
+
+
+    People(){}
+
+    People(int x, String y){
+        age = x;
+        name = y;
+
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    String getName(){
+        return name;
+    }
+    
+
+};
